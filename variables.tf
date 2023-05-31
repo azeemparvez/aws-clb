09d8bd472d50e2a5faf6765437e303b831a1a14e
Azeemuddin Mohammed <azeem_parvez@yahoo.co.in>
2023-05-31 16:11:55 +0100

    aws clb code basic, no NAT Gateway

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "vpc-cidr" {
+  default     = ""
+  type        = string
+  description = "CIDR block details for main VPC"
+}
+
+variable "pusub-1" {
+  default     = ""
+  type        = string
+  description = "public subnet 1 CIDR"
+
+}
+
+variable "pusub-2" {
+  default     = ""
+  type        = string
+  description = "public subnet 2 CIDR"
+
+}
+
+variable "pusub-3" {
+  default     = ""
+  type        = string
+  description = "public subnet 3 CIDR"
+
+}
+
+variable "internet" {
+  default     = ""
+  type        = string
+  description = "Internet IP address"
+
+}
+
+variable "ami" {
+  default     = ""
+  type        = string
+  description = "ami"
+
+}
